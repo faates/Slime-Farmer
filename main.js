fba944818cc35dc445938b487cf979440d357bf2
Russell May <russmay99@gmail.com>
2019-10-10 17:44:28 -0400

    V0.15
    testing things... WIPIPWIPWIPWIPW

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
 var saveData = {
   count: 0,
+  jellies: 0,
   countPerClick: 1,
   countPerClickCost: 10,
-  countClicker: 0,
+  countClicker: 1,
   countClickerCost: 100
 }
 
 function countTwo() {
-  saveData.count += saveData.countPerClick
-  document.getElementById("totalCount").innerHTML = saveData.count + " Count"
+  saveData.count += saveData.jellies * saveData.countClicker
+  saveData.jellies = 0
+  document.getElementById("Green Jellies").innerHTML = saveData.jellies + " Green Jellies"
+  document.getElementById("totalCount").innerHTML = "$" + saveData.count
 }
 
 function countOne() {
-  saveData.count += saveData.countClicker
-  document.getElementById("totalCount").innerHTML = saveData.count + " Count"
+  saveData.jellies += saveData.countPerClick
+  document.getElementById("Green Jellies").innerHTML = saveData.jellies + " Green Jellies"
 }
 
 function buyCountTick() {
   if (saveData.count >= saveData.countPerClickCost) {
     saveData.count -= saveData.countPerClickCost
-    saveData.countPerClick += 1
-    saveData.countPerClickCost += 12
-    document.getElementById("totalCount").innerHTML = saveData.count + " Count"
-    document.getElementById("countUp").innerHTML = "Upgrade Clicker (Lvl " + saveData.countPerClick + ") Cost: " + saveData.countPerClickCost
+    saveData.countClicker += 1
+    saveData.countPerClickCost = Math.floor(saveData.countPerClickCost * 1.75)
+    document.getElementById("totalCount").innerHTML = '$' + saveData.count
+    document.getElementById("countUp").innerHTML = "Upgrade Jelly Refinery (Lvl: " + saveData.countClicker + ") Cost: " + saveData.countPerClickCost
   }
 }
 
 function buyClicker() {
   if (saveData.count >= saveData.countClickerCost) {
     saveData.count -= saveData.countClickerCost
-    saveData.countClickerCost += 22
-    saveData.countClicker += 1
-    document.getElementById("totalCount").innerHTML = saveData.count + " Count"
-    document.getElementById("clickerUp").innerHTML = "Buy a Counter (Lvl " + saveData.countClicker + ") Cost: " + saveData.countClickerCost
+    saveData.countClickerCost = Math.floor(saveData.countClickerCost * 1.95)
+    saveData.countPerClick += 1
+    document.getElementById("totalCount").innerHTML = saveData.count + " Jellies"
+    document.getElementById("clickerUp").innerHTML = "Buy a Green Slime (No: " + saveData.countPerClick + ") Cost: " + saveData.countClickerCost
   }
 }
 
@@ -40,6 +43,7 @@ var mainLoop = window.setInterval(function() {
   countOne()
 }, 1000)
 
+//Saves are commented out for a little while until I have a more complete 'thing'
 /*
 var saveThing = JSON.parse(localStorage.getItem("countSave"))
 if (saveThing !== null) {
